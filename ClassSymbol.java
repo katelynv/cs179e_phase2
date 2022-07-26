e7e27e1222c5ee461239b4491a2f3554f016e10d
katelynv <katelynvi@gmail.com>
2022-07-26 12:41:12 -0700

    add vtable to ClassSymbol

--- ClassSymbol.java
+++ ClassSymbol.java
@@ -1,15 +1,65 @@
-import java.util.HashMap;
-import java.util.Set;
+import java.util.*;
 
 public class ClassSymbol {
     private Symbol name;
     private HashMap<Symbol, String> variables; 
     private HashMap<String, FunctionSymbol> functions; 
+    private LinkedHashMap<String, Integer> vtable;
+    private Integer vtable_offset;
+    private LinkedHashMap<String, Integer> recordtable;
+    private Integer record_offset;
 
     public ClassSymbol (String s) {
         name = Symbol.symbol(s);
         variables = new HashMap<Symbol, String>();
         functions = new HashMap<String, FunctionSymbol>();
+        this.vtable = new LinkedHashMap<String, Integer>();
+        this.recordtable = new LinkedHashMap<String, Integer>();
+        vtable_offset = 0;
+        record_offset = 4;
+    }
+
+    public Integer getvtable_offset(String s) {
+        Integer temp = 0;
+        if (vtable.containsKey(s)) {
+            temp = vtable.get(s);
+        }
+        return temp;
+    }
+
+    public HashMap<Symbol, String> getVariales() {
+        return this.variables;
+    }
+
+    public void setVariables(HashMap<Symbol, String> h) {
+        this.variables = h;
+    }
+
+    public LinkedHashMap<String, Integer> getRecordTable() {
+        return this.recordtable;
+    }
+
+    public int recordTableSize() {
+        return recordtable.size();
+    }
+
+    public void setRecordTable(LinkedHashMap<String, Integer> h) {
+        this.recordtable = h;
+    }
+
+    public LinkedHashMap<String, Integer> getVTable() {
+        return vtable;
+    }
+
+    public boolean checkRecordTable(String s) {
+        if (recordtable.containsKey(s)) {
+            return true;
+        }
+        return false;
+    }
+
+    public Integer getRecordOffset(String s) {
+        return recordtable.get(s);
     }
 
     public String getClassName() {
@@ -18,6 +68,8 @@ public class ClassSymbol {
 
     public void addFunction(String name, String type) {
         this.functions.put(name, new FunctionSymbol(name, type));
+        vtable.put(name,vtable_offset);
+        vtable_offset += 4;
     }
 
     public void addVariables(String name, String type) {
@@ -34,6 +86,10 @@ public class ClassSymbol {
 
     public int functionSize() {
         return functions.size();
+    }
+
+    public int variableSize() {
+        return variables.size();
     }
 
     public Set<String> getFunctionNames() {
